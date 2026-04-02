b748900f1dbc717874ad56e266b23c3b69d9e180
kowshiravichandran <kowshiravichandran@gmail.com>
2026-04-02 18:28:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Submission = require('./models/Submission');
 const Assignment = require('./models/Assignment');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB
 mongoose.connect('mongodb://localhost:27017/assignmentportal')
